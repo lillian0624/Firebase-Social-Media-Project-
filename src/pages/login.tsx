5d994a7ba2df5553b06aa46b3871379db6e6c585
lillian0624 <nopeyangli@163.com>
2023-10-18 23:32:59 +1100

    Authentication

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,14 @@
 import {auth, provider} from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export const Login =()=>{
-
+const navigate = useNavigate();
 
     const signInWithGoogle= async()=>{
         const result = signInWithPopup(auth, provider);
-        console.log(result)
+        console.log(result);
+        navigate('/') //after logined, automaticallly transfer to home page
     }
     return <div><p>Sign In With Google to continue</p>
     <button onClick={signInWithGoogle}>Sign in With Google</button>
