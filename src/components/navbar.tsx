b6b92940371b05cf0f88f9d92fd74b60020785cc
lillian0624 <nopeyangli@163.com>
2023-10-18 23:36:11 +1100

    sigin

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,13 +5,14 @@ import {useAuthState} from "react-firebase-hooks/auth";
 export const Navbar =()=>{
 
     const [user]= useAuthState(auth);
-    return <div>
+    return <div className="navbar">
+        <div className="links">
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
-
-        <div>
+        </div>
+        <div className="user">
             <p>{user?.displayName}</p>
-            <img src={user?.photoURL || ""} width="100" height="100"/>
+            <img src={user?.photoURL || ""} width="20" height="20"/>
         </div>
     </div>
 }
