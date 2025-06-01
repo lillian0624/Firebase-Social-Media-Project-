0fc0cde04a3b1dfdedb7267589f59e46806e1547
Lillian Yang <nopeyangli@163.com>
2025-06-02 00:07:56 +1000

    Update deployment files for latest build

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import './App.css';
 import { Main } from './pages/main/main';
 import { Login } from './pages/login';
 import { Navbar } from './components/navbar';
 import { CreatePost } from './pages/create-post/create-post';
-// import { PostsList } from "./pages/main/PostsList";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar/>
-        
-        <Routes>
-          <Route path="/" element={<Main />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/createpost" element={<CreatePost />}/>
-        </Routes>
-      </Router>
-     
+      <GoogleOAuthProvider clientId="665023975238-ateqcnf8nttn46loh7ug4206i57ijdn8.apps.googleusercontent.com.apps.googleusercontent.com">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/createpost" element={<CreatePost />} />
+          </Routes>
+        </Router>
+      </GoogleOAuthProvider>
     </div>
   );
 }
